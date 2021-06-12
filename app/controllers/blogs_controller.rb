627b1bc8e9d235dfc9953229f962138b9bdd9a26
matusue <jack322437@gmail.com>
2021-06-12 21:49:13 +0000

    {finish}4-5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,8 @@
 class BlogsController < ApplicationController
 
   def show
+    @blog = Blog.find(params[:id])
+    
   end
   
   def index
@@ -16,6 +18,7 @@ class BlogsController < ApplicationController
     blog = Blog.new(blog_params)
     blog.save
     redirect_to blogs_path
+    redirect_to blog_path(blog.id)
   end  
 
   def edit
